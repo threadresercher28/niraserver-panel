848fd8f270747b315d27f5de1ff4b52d7a07eabf
Кирилл <threadresercher@atomicmail.io>
2026-06-21 22:17:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (!defined('panel_password')) {
 <meta name="robots" content="noindex, nofollow, nosnippet, noarchive, noimageindex">
 <title>IPTV - Вход</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="res/font/font-awersome.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
 <style>
 * {
